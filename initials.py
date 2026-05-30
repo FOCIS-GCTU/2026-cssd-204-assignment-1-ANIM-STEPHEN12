88e436a3d7a0e08ecba0c2c2ee6c2de06abb9375
ANIM-STEPHEN12 <2425400816@live.gctu.edu.gh>
2026-05-30 16:09:57 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+
+# Name: ANIM STEPHEN
+# STUDENT ID: 2425400816
+# Email: 2425400816@live.gctu.edu.gh
+# Grader: MR. AUGUSTUS BUCKMAN
+#
+# On my honor, ANIM STEPHEN, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # PRINT MY INITIALS IN THIS STYLE FORMAT
+    print()
+    print("...ASA")
+    print()
 
+    print("...AAAAAAAAAAAA****SSSSSSSSSSSS****AAAAAAAAAAAA")
+    print("...AAAA....AAAA****SSSS........****AAAA....AAAA")
+    print("...AAAA....AAAA****SSSS........****AAAA....AAAA")
+    print("...AAAAAAAAAAAA****SSSSSSSSSSSS****AAAAAAAAAAAA")
+    print("...AAAAAAAAAAAA****........SSSS****AAAAAAAAAAAA")
+    print("...AAAA....AAAA****........SSSS****AAAA....AAAA")
+    print("...AAAA....AAAA****SSSSSSSSSSSS****AAAA....AAAA")
+    print("...AAAA....AAAA****SSSSSSSSSSSS****AAAA....AAAA")
+    print("...AAAA....AAAA****SSSSSSSSSSSS****AAAA....AAAA")
+    print("...AAAA....AAAA****SSSSSSSSSSSS****AAAA....AAAA")
 
-
+    print()
+    print()
 main()
